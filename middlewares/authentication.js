dccd09c0eb0dd7a32999fe7f977550c56c1a4ec6
alexanderbry <alexunderbrain@gmail.com>
2024-10-09 23:14:24 +0700

    Fix feature controller

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -2,11 +2,8 @@ const { verifyToken } = require("../helpers/jwt");
 
 function authentication(req, res, next) {
   try {
-    console.log("---");
-
     if (!req.headers.authorization) throw { name: "Unauthorized" };
     const [bearer, token] = req.headers.authorization?.split(" ");
-    console.log("<<<<<<<");
 
     const verify = verifyToken(token);
     if (!verify) throw { name: "Unauthorized" };
--- models/user.js
+++ models/user.js
@@ -5,6 +5,7 @@ const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     static associate(models) {
+      
       User.hasOne(models.Profile);
     }
   }
